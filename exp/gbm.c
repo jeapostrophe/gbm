444d5589353fa74f9b0ab72728beeb9ffe44d275
Jay McCarthy <jay.mccarthy@gmail.com>
2016-04-12 18:56:50 -0400

    up

--- exp/gbm.c
+++ exp/gbm.c
@@ -1,14 +1,17 @@
 #include <string.h>
 
 extern int audio_main();
+extern int audio2_main();
 // extern int jit_main();
 extern int thread_main();
 extern int video_main();
 extern int vm_main();
 
 int main(int argc, char **argv) {
-         if ( strcmp(argv[1], "audio") == 0) {
+  if ( strcmp(argv[1], "audio") == 0) {
     audio_main();
+  } else if ( strcmp(argv[1], "audio2") == 0) {
+    audio2_main();
   } else /*if ( strcmp(argv[1], "jit") == 0) {
     jit_main();
     } else */ if ( strcmp(argv[1], "thread") == 0) {
