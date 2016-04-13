8a1e53b7bcd5af038c89f735423cb58c2006f0bd
Jay McCarthy <jay.mccarthy@gmail.com>
2016-04-13 19:45:26 -0400

    progress, but still broken

--- cl/h/libc.rkt
+++ cl/h/libc.rkt
@@ -15,18 +15,21 @@
 (define sinf ($extern <math.h> "sinf"))
 
 (define (check-zero e #:m [mf (λ (err) (list "non-zero return code: %d\n" err))])
+  ;; XXX i wish i could inspect the type of e
   ($let1 ([SI32 err e])
          ($unless ($== ($v SI32 0) err)
                   (apply $fprintf $stderr (mf err))
                   ($exit 1))))
 
 (define (check-pos e #:m [mf (λ (err) (list "non-pos return code: %d\n" err))])
+  ;; XXX i wish i could inspect the type of e
   ($let1 ([SI32 err e])
          ($unless ($< ($v SI32 0) err)
                   (apply $fprintf $stderr (mf err))
                   ($exit 1))))
 
 (define (check-null e #:m [m "null pointer!\n"])
+  ;; XXX i wish i could inspect the type of e
   ($when ($== e ($v (Ptr Any) $NULL))
          ($fprintf $stderr m)
          ($exit 1)))
